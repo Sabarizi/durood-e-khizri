ff649da117a29b3c7d8a970ebff3f4d54d1abf68
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2026-06-17 13:05:34 +0500

    add

--- firebase/config.ts
+++ firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmmbFvsrZdwgSJ-7qJIRD7kMFk3UKuy6o",
-  authDomain: "durood-e-khizri.firebaseapp.com",
-  projectId: "durood-e-khizri",
-  storageBucket: "durood-e-khizri.appspot.com",
-  messagingSenderId: "145767240151",
-  appId: "1:145767240151:web:eb6c0db35c21eb50f6fb92",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
